e4334eb269d4009832eb884731477f09ba93cf31
Nicollas Cosmo <nicollascosmo@hotmail.com>
2023-04-25 12:43:02 -0300

    last update sobreMim

--- src/pages/Sobremim/index.js
+++ src/pages/Sobremim/index.js
@@ -22,17 +22,16 @@ export default function SobreMim() {
                 className={styles.fotoSobreMim}
             />
 
-            <p className={styles.paragrafo}>Oi, tudo bem? Eu sou instrutor de Front-end na Alura e estou feliz de te ver por aqui.</p>
+            <p className={styles.paragrafo}>Sou um desenvolvedor front-end com experiência em projetos de grande escala para orgãoes públicos e empresas líderes no mercado. Possuo amplo conhecimento em HTML, CSS, JavaScript, TypeScript, ReactJS e React Native. Sou especializado em desenvolvimento de interfaces de usuário e tenho experiência em integração de API e teste automatizado.</p>
 
-            <p className={styles.paragrafo}>Minha história com programação começou no Instituto Federal do Piauí (IFPI), quando fiz o ensino médio integrado ao curso de Informática. Eu aprendi lógica de programação e o básico de várias linguagens, como PHP, Python, Java, mas sem aprofundar muito em cada uma delas. Eu gostava muito de estudar programação, mas na época não fazia ideia de que trabalharia com isso hoje.</p>
+            <p className={styles.paragrafo}>Em meus projetos anteriores, tive a oportunidade de trabalhar com equipes multidisciplinares e contribuir significativamente para o sucesso do produto final. Em minha última experiência,ajudei no desenvolvimento de uma nova aplicação mobile para um grande orgão público que resultou em um aumento de 20% na produtividade.</p>
 
-            <p className={styles.paragrafo}>No ensino superior, escolhi cursar Engenharia Elétrica na Universidade Federal do Piauí (UFPI). Lá eu consegui entrar no PET (Programa de Educação Tutorial), um grupo acadêmico que contribui de diversas formas para o curso. Lá eu tive a oportunidade de estudar desenvolvimento web para ser um dos mantenedores do site deles.</p>
+            <p className={styles.paragrafo}>Tenho certificação em ReactJs e React Native, além de ter concluído vários cursos de desenvolvimento web e design responsivo. Estou sempre em busca de novas tecnologias e técnicas para melhorar minhas habilidades.</p>
 
-            <p className={styles.paragrafo}>Com isso tive minha experiência de dev mais próxima da realidade, com prazos de implementação para o site, e aprendi muito enquanto codificava.</p>
+            <p className={styles.paragrafo}>Meu objetivo é trabalhar em uma empresa que valorize a qualidade do produto e a colaboração entre equipes. Estou animado para desenvolver soluções de front-end inovadoras que contribuam para o crescimento de empresas em vários seguimentos.</p>
 
-            <p className={styles.paragrafo}>E foi enquanto eu estava no PET que decidi procurar um estágio em desenvolvimento web. Eu encontrei uma vaga na Alura para o Scuba Team e percebi que ela aliava duas paixões minhas: programação e educação. Para minha felicidade, fui escolhido para fazer parte do time.</p>
+            <p className={styles.paragrafo}>https://github.com/NicollasCosmo</p>
 
-            <p className={styles.paragrafo}>Desde então, tem sido aprenas aprendizados atrás de aprendizados. A Alura é uma escola não só para seus alunos e alunas, mas também para os colaboradores e colaboradoras. Hoje sou muito feliz de ter a oportunidade de trazer esses conteúdos ricos e encantadores para você. Espero que aprenda bastante!</p>
 
         </PostModelo>
     )
